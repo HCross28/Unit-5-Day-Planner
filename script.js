b0c9efb4e3dcddc4688ef3b63b80a8511cc81be1
HCDesktop <HaydenLCrossJunk@gmail.com>
2019-11-23 00:54:46 -0800

    began js, only have partial lock change

---
+++ script.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+var btn = document.getElementsByTagName("button");
+console.log(btn.length);
+
+
+$(btn).click(function(){
+    $(this).html("<i class='fas fa-lock'></i>");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
